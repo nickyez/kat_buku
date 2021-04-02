fe35c90cb326a13e5cbef974b0d66c8a8ce98671
Yulisa Dewi Musarofah <80933455+yulisa12345@users.noreply.github.com>
2021-04-02 14:01:05 +0700

    menambahkan backend di penerbit
    mengumpulkan project web - penerbit - Yulisa Dewi Musarofah_041

--- admin/editpenerbit.php
+++ admin/editpenerbit.php
@@ -1,3 +1,20 @@
+<?php 
+  session_start();
+  include('../koneksi/koneksi.php');
+  if(isset($_GET['data'])){
+    $id_penerbit = $_GET['data'];
+    $_SESSION['id_penerbit']=$id_penerbit;
+
+    //get data penerbit
+    $sql_d = "select `penerbit`, `alamat` from `penerbit` where `id_penerbit` = '$id_penerbit'";
+    $query_d = mysqli_query($koneksi,$sql_d);
+    while($data_d = mysqli_fetch_row($query_d)){
+    $penerbit= $data_d[0];
+    $alamat= $data_d[1];
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,7 +37,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="katgoribuku.php">Penerbit</a></li>
+              <li class="breadcrumb-item"><a href="penerbit.php">Penerbit</a></li>
               <li class="breadcrumb-item active">Edit Penerbit</li>
             </ol>
           </div>
@@ -35,22 +52,24 @@
       <div class="card-header">
         <h3 class="card-title"style="margin-top:5px;"><i class="far fa-list-alt"></i> Form Edit Penerbit</h3>
         <div class="card-tools">
-          <a href="penerbit.php" class="btn btn-sm btn-warning float-right">
-          <i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
+          <a href="penerbit.php" class="btn btn-sm btn-warning float-right"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
         </div>
       </div>
       <!-- /.card-header -->
       <!-- form start -->
       </br>
-      <div class="col-sm-10">
-          <div class="alert alert-danger" role="alert">Maaf data Penerbit wajib di isi</div>
-      </div>
-      <form class="form-horizontal">
+      <?php if(!empty($_GET['notif'])){?>
+      <?php if($_GET['notif']=="editkosong") {?>
+        <div class="alert alert-danger" role="alert">
+        Maaf data penerbit wajib diisi</div>
+        <?php }?>
+      <?php }?>
+      <form class="form-horizontal" method="post" action="konfirmasieditpenerbit.php">
         <div class="card-body">
           <div class="form-group row">
             <label for="Penerbit" class="col-sm-3 col-form-label">Penerbit</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control" id="Penerbit" value="Andi">
+              <input type="text" class="form-control" id="Penerbit" name="penerbit" value="<?php echo $penerbit; ?>">
             </div>
           </div>
           <div class="form-group row">
