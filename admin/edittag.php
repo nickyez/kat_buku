cbd3d89eb21a5a2143d36167d91cc4a6bf8aa3bb
Novandre <82073107+Novandre@users.noreply.github.com>
2021-04-07 10:37:16 +0700

    menambahkan backend pada menu edit tag
    mengumpulkan project web - edit tag - Novandre

--- admin/edittag.php
+++ admin/edittag.php
@@ -2,6 +2,21 @@
 <html>
 <head>
 <?php include("includes/head.php") ?> 
+<?php 
+session_start();
+include('../koneksi/koneksi.php');
+if(isset($_GET['data'])){
+	$id_tag = $_GET['data'];
+	$_SESSION['id_tag']=$id_tag;
+	
+	//get data tag
+	$sql_a = "select `tag` from `tag` where `id_tag` = '$id_tag'";
+	$query_a = mysqli_query($koneksi,$sql_a);
+	while($data_a = mysqli_fetch_row($query_a)){
+		$tag = $data_a[0];
+	}
+}
+?>
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -42,14 +57,18 @@
       <!-- form start -->
       </br>
       <div class="col-sm-10">
-          <div class="alert alert-danger" role="alert">Maaf data Tag wajib di isi</div>
+		<?php if(!empty($_GET['notif'])){?>
+			<?php if($_GET['notif']=="editkosong"){?>
+			<div class="alert alert-danger" role="alert">Maaf data Tag wajib di isi</div>
+			<?php }?>
+		<?php }?>
       </div>
-      <form class="form-horizontal">
+      <form class="form-horizontal" method="post" action="konfirmasiedittag.php">
         <div class="card-body">
           <div class="form-group row">
             <label for="Tag" class="col-sm-3 col-form-label">Tag</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control" id="Tag" value="PHP">
+              <input type="text" class="form-control" id="tag" Name="tag" value="<?php echo $tag;?>">
             </div>
           </div>
         </div>
