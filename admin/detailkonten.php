a415528cca1e2d9f92e04ec8de6b816970988e72
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-05-27 22:42:19 +0700

    Memindahkan file ke folder admin

--- admin/detailkonten.php
+++ admin/detailkonten.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html> 
-</head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
-
-  <?php include("includes/sidebar.php") ?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h3><i class="fas fa-user-tie"></i> Detail Data Konten</h3>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item"><a href="konten.php">Data Konten</a></li>
-              <li class="breadcrumb-item active">Detail Data Konten</li>
 <?php
   session_start();
   include('../koneksi/koneksi.php');
--- admin/editkonten.php
+++ admin/editkonten.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<?php include("includes/head.php") ?> 
-</head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
-
-  <?php include("includes/sidebar.php") ?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h3><i class="fas fa-edit"></i> Edit Data Konten</h3>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="konten.php">Data Konten</a></li>
-              <li class="breadcrumb-item active">Edit Data Konten</li>
-            </ol>
 <?php
   session_start();
   include('../koneksi/koneksi.php');
